c80d64873e392c5dff43546675c87094f720c721
Demur Rumed <gunkmute@gmail.com>
2011-08-25 17:35:52 +0000

    Halved texture size. Fixed up untested objects. Etc

--- j.c
+++ j.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-#include <stdint.h>
 #include <string.h>
 #include <GL/glfw.h>
 #include "tgen.h"
@@ -14,7 +13,7 @@ typedef struct Obj{
 	unsigned char d[];
 }Obj;
 Obj*P;
-int Pd,Pu,Pk[2],Kv,Kh;
+int Pd,Pu,Pk[2],Kv,Kh,Kp;
 Obj*makeObj(char t,float x,float y,int d){
 	Obj*r=malloc(d+sizeof(Obj));
 	r->t=t;
@@ -26,15 +25,16 @@ typedef struct{
 	short w,h;
 	char*s;
 }Lt;
-Lt L0={7,7,
-"!:    -"
-" #=-@  "
-"   #;-%"
-"  v ,- "
-"    ^-="
-"   .-  "
-"   #  -"},
-L1={2,19,
+Lt L[]={{12,8,
+"         v  "
+"!:     ((-))"
+" #=-@    -  "
+"   #;-%>>   "
+"  v ,- ))   "
+" ^  ^-= <<  "
+" ^-.-  ((  #"
+"   #^ ----> "},
+{2,19,
 "@ "
 "vv"
 ".,"
@@ -53,24 +53,26 @@ L1={2,19,
 "%:"
 ": "
 ";!"
-": "},*LL;
+": "},{1,1,"@"}},*LL=L;
 
-void drawRect(float x,float y,float tx,float ty){
-	tx/=8;
-	ty/=8;
-	glTexCoord2f(tx,ty);
-	glVertex2f(x,y);
-	glTexCoord2f(tx+1./8,ty);
-	glVertex2f(x+1,y);
-	glTexCoord2f(tx+1./8,ty+1./8);
-	glVertex2f(x+1,y+1);
-	glTexCoord2f(tx,ty+1./8);
-	glVertex2f(x,y+1);
+void drawRect_(float x,float y,float a,float b,float h,float v){
+	a/=8;
+	b/=4;
+	glTexCoord2f(a,b);
+	glVertex2f(x+h,y+v);
+	glTexCoord2f(a+1./8,b);
+	glVertex2f(x+1-h,y+v);
+	glTexCoord2f(a+1./8,b+1./4);
+	glVertex2f(x+1-h,y+1-v);
+	glTexCoord2f(a,b+1./4);
+	glVertex2f(x+h,y+1-v);
+}
+void drawRect(float x,float y,float a,float b){
+	drawRect_(x,y,a,b,0,0);
 }
 
 void loadL(Lt*L){
 	Obj*h=P;
-	LL=L;
 	for(int x=0;x<L->w;x++)
 		for(int y=0;y<L->h;y++){
 			int t;
@@ -88,16 +90,26 @@ void loadL(Lt*L){
 				h=h->n=makeObj(5,x,y,2);
 				h->d[0]=0;
 				h->d[1]=255;
-			case('v')h=h->n=makeObj(6,x,y,0);
-			case('^')h=h->n=makeObj(7,x,y,0);
-			case('(')h=h->n=makeObj(8,x,y,0);
-			case(')')h=h->n=makeObj(9,x,y,0);
+			case('v')
+				h=h->n=makeObj(6,x,y,1);
+				h->d[0]=0;
+			case('^')
+				h=h->n=makeObj(6,x,y,1);
+				h->d[0]=1;
+			case('(')
+				h=h->n=makeObj(8,x,y,1);
+				h->d[0]=0;
+			case(')')
+				h=h->n=makeObj(8,x,y,1);
+				h->d[0]=1;
 			case('<')
-				h=h->n=makeObj(10,x,y,1);
+				h=h->n=makeObj(10,x,y,2);
 				h->d[0]=0;
+				h->d[1]=1;
 			case('>')
-				h=h->n=makeObj(11,x,y,1);
+				h=h->n=makeObj(10,x,y,2);
 				h->d[0]=0;
+				h->d[1]=0;
 			case('.')
 				h=h->n=makeObj(12,x,y,1);
 				h->d[0]=0;
@@ -163,44 +175,46 @@ int main(int argc,char**argv){
 	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
 	glTexImage2D(GL_TEXTURE_2D,0,gF,gW,gH,0,gF,GL_UNSIGNED_BYTE,g);
 	P=makeObj(0,0,0,0);
-	loadL(&L0);
+	loadL(LL);
 	for(;;){
-		if(P->y>64)loadL(LL);
+		if(P->y>64||glfwGetKey(GLFW_KEY_SPACE))loadL(LL);
 		glClear(GL_COLOR_BUFFER_BIT);
 		glBegin(GL_QUADS);
 		Kv=glfwGetKey(GLFW_KEY_DOWN)-glfwGetKey(GLFW_KEY_UP);
 		Kh=glfwGetKey(GLFW_KEY_RIGHT)-glfwGetKey(GLFW_KEY_LEFT);
+		if(Kh)Kp=Kh==1;
 		for(Obj*n=P;n;n=n->n){
 			gomo:switch(n->t){
 			Obj*o;
 			case(0)
-				if(Pu!=4&&Kh){
-					Pd=Kh;
-					goto walk;
-				}
 				if(n->x!=(int)n->x){
-					walk:if(!(oat(2,n->x+Pd,n->y)||oat(3,n->x+Pd,n->y))){
-						if(o=oat(13,n->x+Pd,n->y)){
+					walk:n->x+=Pd/8.;
+				}else if(Pu!=4&&((o=oat(10,n->x,n->y+1))||Kh)){
+					Pd=Kh;
+					if(o)Pd+=o->d[1]?-1:1;
+					int pd=Pd>0?1:Pd<0?-1:0;
+					if(!((o=oat(8,n->x+pd,n->y))&&pd!=(o->d[0]?:-1)||oat(2,n->x+pd,n->y)||oat(3,n->x+pd,n->y)||oat(10,n->x+pd,n->y))){
+						if(o=oat(13,n->x+pd,n->y)){
 							if(Pk[o->d[0]]){
 								Pk[o->d[0]]--;
 								del(o);
 							}else goto nokey;
 						}
-						n->x+=Pd/8.;
-						nokey:;
+						goto walk;
 					}
-				}else if(Kv&&((oat(5,n->x,n->y)&&!any(n->x,n->y+Kv))||(oat(4,n->x,n->y))&&!(oat(Kv==1?7:6,n->x,n->y+Kv)||oat(10,n->x,n->y+Kv)||oat(11,n->x,n->y+Kv)||oat(13,n->x,n->y+Kv)))){
+					nokey:;
+				}else if(Kv&&((oat(5,n->x,n->y)&&!any(n->x,n->y+Kv))||(oat(4,n->x,n->y))&&!(((o=oat(6,n->x,n->y+Kv))&&Kv==(o->d[0]?:-1))||oat(10,n->x,n->y+Kv)||oat(13,n->x,n->y+Kv)))){
 					Pu=Kv;
 					goto vert;
-				}else if(Kv==-1&&oat(7,n->x,n->y)){
+				}else if(Kv==-1&&(o=oat(6,n->x,n->y))&&o->d[0]==1){
 					Pu=-1;
 					goto vert;
-				}else if(Kv==1&&(oat(6,n->x,n->y)||oat(6,n->x,n->y+1)||(oat(4,n->x,n->y+1)&&!(oat(5,n->x,n->y)||oat(7,n->x,n->y))))){
+				}else if(Kv==1&&(((o=oat(6,n->x,n->y))&&o->d[0]==0||(o=oat(6,n->x,n->y+1))&&o->d[0]==0)||(oat(4,n->x,n->y+1)&&!(oat(5,n->x,n->y)||((o=oat(6,n->x,n->y))&&o->d[0]==1))))){
 					Pu=1;
 					goto vert;
 				}
 				if(oat(1,n->x,n->y)){
-					loadL(&L1);
+					loadL(++LL);
 				}
 				if(o=oat(3,n->x,n->y+1)){
 					if(!(o->d[0]+=4))
@@ -230,14 +244,13 @@ int main(int argc,char**argv){
 					n->y+=Pu/8.;
 				}else if(n->x==(int)n->x&&!any(n->x,n->y+1)&&!(oat(4,n->x,n->y)||oat(5,n->x,n->y)||oat(6,n->x,n->y)||oat(7,n->x,n->y)||oat(8,n->x,n->y)||oat(9,n->x,n->y))){
 					Pu=4;
-					Pd=0;
 					goto vert;
 				}else Pu=0;
-				drawRect(n->x,n->y,Pd==1,0);
-			case(1)drawRect(n->x,n->y,3,0);
-			case(2)drawRect(n->x,n->y,2,0);
-			case(3)drawRect(n->x,n->y,n->d[0]>>5,3);
-			case(4)drawRect(n->x,n->y,4,0);
+				drawRect_(n->x,n->y,0,0,Kp,0);
+			case(1)drawRect(n->x,n->y,2,0);
+			case(2)drawRect(n->x,n->y,1,0);
+			case(3)drawRect(n->x,n->y,n->d[0]>>5,2);
+			case(4)drawRect(n->x,n->y,3,0);
 			case(5)
 				int y=n->y;
 				n->d[0]-=n->d[0]>0;
@@ -267,15 +280,12 @@ int main(int argc,char**argv){
 				glEnd();
 				glBindTexture(GL_TEXTURE_2D,Ts);
 				glBegin(GL_QUADS);
-				drawRect(n->x,n->y,6,4);
-			case(6)drawRect(n->x,n->y,7,0);
-			case(7)drawRect(n->x,n->y,7,1);
-			case(8)drawRect(n->x,n->y,5,0);
-			case(9)drawRect(n->x,n->y,6,0);
-			case(10)drawRect(n->x,n->y,4,n->d[0]++/51);
-			case(11)drawRect(n->x,n->y,4,n->d[0]--/51);
+				drawRect(n->x,n->y,6,0);
+			case(6)drawRect_(n->x,n->y,5,0,0,n->d[0]);
+			case(8)drawRect_(n->x,n->y,4,0,n->d[0],0);
+			case(10)drawRect(n->x,n->y,(n->d[0]+=(n->d[1]?4:-4))/43,3);
 			case(12)drawRect(n->x,n->y,n->d[0],1);
-			case(13)drawRect(n->x,n->y,n->d[0],2);
+			case(13)drawRect(n->x,n->y,4+n->d[0],1);
 			case(14)
 				glEnd();
 				glBindTexture(GL_TEXTURE_2D,0);
